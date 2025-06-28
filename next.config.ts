0d120fb297f1cd950ca6bef1a31080e5fbd8311a
seasalt-haiyan <haoyang05.xu@gmail.com>
2025-06-28 21:39:52 +0800

    build: 在构建时忽略 ESLint 错误

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 在构建时忽略 ESLint 错误
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
